f79a37d1ec8e285a80b0d8479137846b141fac21
mgrecu <mgrecu@pcp513464pcs.ndc.nasa.gov>
2020-08-28 09:24:52 -0400

    Aug 28,2020

--- readSubset.py
+++
@@ -1,4 +0,0 @@
-from netCDF4 import Dataset
-fh=Dataset("subSets/subSet_wrfout_d04_20140611_15:00-16:00.nc","r")
-dbz=fh.variables.get("dbz")
-
--- temp.py
+++
@@ -1,4 +0,0 @@
-zKu=fh['NS/PRE/zFactorMeasured'][i0,j0+12,:]
-zKa=fh['NS/PRE/zFactorMeasured'][i0,j0,:]
-clutF=fh['NS/PRE/binClutterFreeBottom'][i0,j0+12]
-fh['NS/SRT/reliabFlag'][i0,j0+12]
